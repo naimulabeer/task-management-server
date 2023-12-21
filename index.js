08499c9a4938d8d84e9f758cd61b2c3f1ba8c069
naimulabeer <naimul.h.abeer@gmail.com>
2023-12-22 01:31:13 +0600

    post route for task added

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygqixms.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const taskCollection = client.db("taskTrello").collection("tasks");
+
+    app.post("/tasks", async (req, res) => {
+      try {
+        const taskItem = req.body;
+        await taskCollection.insertOne(taskItem);
+        res.send(taskItem);
+      } catch (error) {
+        console.error("Error adding work item:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Task Trello Server running");
+});
+
+app.listen(port, () => {
+  console.log(`Task Trello listening from port ${port}`);
+});
